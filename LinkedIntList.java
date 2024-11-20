c6859467a5ca2df7111fc73d9a5d4fbd49a397b7
Arnav Jain <154857811+Dark-Knight-180@users.noreply.github.com>
2024-11-20 10:19:26 -0600

    final push hopefully

--- LinkedIntList.java
+++ LinkedIntList.java
@@ -2,73 +2,178 @@
 // LinkedIntList.java
 // Purpose of this lab is to manage all the functions of a linkedlist like add, get, indexOf, and more. This is very important as it gives the linkedlist functionality and allows us to modify the list.
 
-
 public class LinkedIntList {
 
-	ListNode front;
-	ListNode current;
-	
-	public LinkedIntList() {
-		
-		front = null;
-	}
-	
-	public LinkedIntList(int value) {
-		
-		front = new ListNode(value);
-		
-	}
-	
-	public void add(int value) {
-		
-		if(front == null) {
-			
-			front = new ListNode(value);
-			
-		}
-		else {
-			
-			front.next = new ListNode(value);
-			
-		}
-		
-	}
-	
-	public void add(int index, int value) {
-		
-	}
-	
-	public int get(int index) {
-		
-		return index;
-		
-	}
-	
-	public int indexOf (int value) {
-		
-		return value;
-		
-	}
-	
-	public int remove(int index) {
-		
-		return index;
-		
-	}
-	
-	public int size() {
-		
-		return 0;
-		
-	}
-	
-	public String toString( ) {
-		
-		String list = "";
-		while(start.next != null) {
-			list += start
-		}
-		return start.data + ", " + start.next.data;
-		
-	}
+    ListNode front;
+
+    public LinkedIntList() {
+
+        front = null;
+    }
+
+    public LinkedIntList(int value) {
+
+        front = new ListNode(value);
+
+    }
+
+    public void add(int value) {
+
+        if (front == null) {
+
+            front = new ListNode(value);
+
+        } else {
+
+            ListNode current = front;
+
+            while (current.next != null) {
+
+                current = current.next;
+
+            }
+
+            current.next = new ListNode(value);
+
+        }
+
+    }
+
+    public void add(int index, int value) {
+
+        if (index < 0 || index > size() - 1) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0) {
+
+            front = new ListNode(value, front);
+
+        } else {
+
+            ListNode current = front;
+            for (int i = 0; i < size() - 1; i++) {
+                if (i == index - 1) {
+                    current.next = new ListNode(value, current.next);
+                    return;
+                }
+                current = current.next;
+            }
+            current.next = new ListNode(value, current.next);
+
+        }
+
+    }
+
+    public int get(int index) {
+
+        if (index < 0 || index > size() - 1) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0) {
+            return front.data;
+        } else {
+            ListNode current = front;
+            for (int i = 0; i < size() - 1; i++) {
+                if (i == index) {
+                    return current.data;
+                }
+                current = current.next;
+            }
+            return current.data;
+        }
+    }
+
+    public int indexOf(int value) {
+
+        ListNode current = front;
+        for (int i = 0; i < size(); i++) {
+            if (current.data == value) {
+                return i;
+            }
+            current = current.next;
+        }
+        return -1;
+
+    }
+
+    public void remove(int index) {
+
+        if (index < 0 || index > size() - 1) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0) {
+            front = front.next;
+        } else {
+            ListNode current = front;
+            for (int i = 0; i < size() - 1; i++) {
+                if (i == index - 1) {
+                    current.next = current.next.next;
+                    return;
+                }
+                current = current.next;
+            }
+            current.next = current.next.next;
+        }
+    }
+
+    public int size() {
+
+        int size = 1;
+        if (front == null) {
+            return 0;
+        } else {
+            ListNode current = front;
+
+            while (current.next != null) {
+                size++;
+                current = current.next;
+            }
+        }
+        return size;
+    }
+
+    public String toString() {
+
+        ListNode entireList = front;
+
+        String list = "";
+        while (entireList.next != null) {
+            list += entireList.data + ", ";
+            entireList = entireList.next;
+        }
+        list += entireList.data;
+        return list;
+
+    }
+
+    public void clear() {
+
+        front = null;
+
+    }
+
+    public void sort() {
+
+        ListNode current = front;
+        ListNode index = null;
+        int temp;
+
+        if (front == null) {
+        } else {
+            while (current != null) {
+                index = current.next;
+
+                while (index != null) {
+                    if (current.data > index.data) {
+                        temp = current.data;
+                        current.data = index.data;
+                        index.data = temp;
+                    }
+                    index = index.next;
+                }
+                current = current.next;
+            }
+        }
+    }
+
 }
--- LinkedListReplicated.java
+++ LinkedListReplicated.java
@@ -3,9 +3,6 @@
  * 
  */
 
-import java.io.File;
-import java.io.IOException;
-
 
 public class LinkedListReplicated  {
  
@@ -16,7 +13,28 @@ public class LinkedListReplicated  {
     	LinkedIntList trial2 = new LinkedIntList(12);
     	
     	trial2.add(122);
-    	System.out.println(trial2.toString());
+    	trial2.add(534);
+		trial2.add(-756);
+    	trial2.add(886);
+		trial2.add(2,23);
+    	trial2.add(2, -74);
+
+		System.out.println(trial2.toString());
+		System.out.println(trial2.get(6));
+
+		trial2.remove(6);
+		System.out.println(trial2.toString());
+		System.out.println(trial2.get(5));
+		System.out.println(trial2.size());
+		System.out.println(trial2.indexOf(534));
+		trial2.sort();
+		System.out.println(trial2.toString());
+		trial.clear();
+		System.out.println(trial.toString());
+
+
+
+
     	
       	//Test out each of your various methods (add, insert, get, indexOf, remove, size, toString)) 
       		//using either of the instantiated LinkedIntList objects
